0b2bad22cd84ab7e9824b49107a4929f360fb7f0
holma198 <holma198@morris.umn.edu>
2017-09-04 02:38:37 -0500

    Create a cat function to concatinate the three files and an if statement to determine wether the output file exists and a cp function to copy concatinated file into output file.  ERROR occurs in which wrap_contents.sh cannot find files that are given, as arguments.

--- wrap_contents.sh
+++ wrap_contents.sh
@@ -1,3 +1,19 @@
 #!bin/bash
 
-echo "It works"
+$1=contentsFile
+$2=desiredHeaderFoot
+$3=resultFile
+
+##cd ./simple_example
+##echo "hi"
+{cat $desiredHeaderFoot"_header.html" $contentsFile $desiredHeaderFoot"_footer.html"} = finalfile
+
+## to check if file exits in bash credit
+## https://stackoverflow.com/questions/638975/how-do-i-tell-if-a-regular-file-does-not-exist-in-bash
+
+if [ -f $resultFile ]; then
+	cp -f $finalfile $resultFile
+else
+	touch $resultFile
+	cp -f $finalfile $resultFile
+fi
