ebfc9b2290bafe675e6c526ca4ecb1586b9b30b6
holma198 <holma198@morris.umn.edu>
2017-09-05 03:57:13 -0500

    Remove echo lines and fix typo in $3 var name.  This code now works to produce my_chart.html, however tests do not pass.  This may have something to do with the fact this code was written on a linux virtual machine on a windows machine.  The error is something related to not finding a directory.  When running the code sh is needed before running the bashscript, and when researching the issue, lots of sources point to an issue with the shabang statement that will cause this problem.  This will need to be tested in the lab and then tests may pass.

--- wrap_contents.sh
+++ wrap_contents.sh
@@ -7,11 +7,5 @@ desiredHeaderFoot=$2
 ##name of concatinated file that will be the resulting html file.
 resultFile=$3
 
-##cd ./simple_example
-echo "${desiredHeaderFoot}_header.html"
-echo "$contentsFile"
-echo "${desiredHeaderFoot}_footer.html"
-echo "$resultFile"
-
 ##This line concatinates the header contents and footer into a single html file 
-##cat "${desiredHeaderFoot}_header.html" "$contentsFile" "${desiredHeaderFoot}_footer.html" > "$resultFile"
+cat "${desiredHeaderFoot}_header.html" "$contentsFile" "${desiredHeaderFoot}_footer.html" > "$resultFile"
