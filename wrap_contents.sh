d7c23ddaa77e576bfe7bfa24b90a487f7f77756d
holma198 <holma198@morris.umn.edu>
2017-09-05 03:50:22 -0500

    add an echo for each variable to ensure that each string for the cat argument are indeed correct.

--- wrap_contents.sh
+++ wrap_contents.sh
@@ -1,19 +1,17 @@
-#!bin/bash
+#!/usr/bin/ash
 
-$1=contentsFile
-$2=desiredHeaderFoot
-$3=resultFile
+##Name of file whose contents exist in the <p> html tag
+contentsFile=$1
+##name of the header and string (root name) to be concatinated to the contentsFile
+desiredHeaderFoot=$2
+##name of concatinated file that will be the resulting html file.
+resultFile=$3
 
 ##cd ./simple_example
-##echo "hi"
-{cat $desiredHeaderFoot"_header.html" $contentsFile $desiredHeaderFoot"_footer.html"} = finalfile
+echo "${desiredHeaderFoot}_header.html"
+echo "$contentsFile"
+echo "${desiredHeaderFoot}_footer.html"
+echo "$resultFile"
 
-## to check if file exits in bash credit
-## https://stackoverflow.com/questions/638975/how-do-i-tell-if-a-regular-file-does-not-exist-in-bash
-
-if [ -f $resultFile ]; then
-	cp -f $finalfile $resultFile
-else
-	touch $resultFile
-	cp -f $finalfile $resultFile
-fi
+##This line concatinates the header contents and footer into a single html file 
+##cat "${desiredHeaderFoot}_header.html" "$contentsFile" "${desiredHeaderFoot}_footer.html" > "$resultFile"
